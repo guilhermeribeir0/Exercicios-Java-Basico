b6934e69aebc0c34a1f9060472e4da00b509c658
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2021-08-15 17:03:29 -0300

    Adicionado primeiro exercício de Estruturas Condicionais

--- EstruturasCondicionais/Ex01.java
+++ EstruturasCondicionais/Ex01.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Ex01 {
+
+    /*
+    Acesso a um jogo para maiores de 18 anos.
+    Ler a idade do usuárioe retornar se ele pode acessar ou não o jogo.
+    */
 
     public static void main(String[] args) {
 
-        
-        
-    }
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Para poder entrar no jogo, por favor, nos informe a sua idade.");
+        System.out.print(">> Idade: ");
+        int userIdade = input.nextInt();
+
+        if (userIdade >= 18) {
+            System.out.println("========================\nAcesso autorizado! \nDivirta-se!");
+        } else {
+            System.out.println("Acesso negado! \nDesculpe! Você não possui a idade minima.");
+        }
+    }
 }
 
