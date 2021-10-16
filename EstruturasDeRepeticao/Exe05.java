08b4fac9e657ce5d1838ce327f0b9dede2145bd4
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2021-10-16 10:40:19 -0300

    Acrescentado problema e resolução ao arquivo Exe05.

--- EstruturasDeRepeticao/Exe05.java
+++ EstruturasDeRepeticao/Exe05.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Exe05 {
+
+    //Leia a idade de 10 pessoas e exiba a soma das idades. 
 
     public static void main(String[] args) {
 
-        
+        Scanner input = new Scanner(System.in);
+
+        int idade, soma = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("-------------------");
+            System.out.print("\nInforme uma idade: ");
+            idade = input.nextInt();
+
+            soma = soma + idade;
+        }
+
+        System.out.println("--------------------------");
+        System.out.println("A soma das idades é: "+soma);
+        System.out.println("--------------------------");
         
     }
 
